b96963998845ca0ade8bb9804be62762fc406a0c
Maryna Bonko <bonko23@gmail.com>
2023-05-05 12:47:24 +0300

    fixed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -11,16 +11,19 @@ document.body.style.backgroundColor = getRandomHexColor();
 
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
+let intervalId;
 
 startButton.addEventListener('click', () => {
   startButton.disabled = true;
+  stopButton.disabled = false;
 
-  setInterval(() => {
+  intervalId = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
 });
 
 stopButton.addEventListener('click', () => {
-  clearInterval();
+  clearInterval(intervalId);
+  stopButton.disabled = true;
   startButton.disabled = false;
 });
